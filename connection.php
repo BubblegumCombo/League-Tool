153aa3bbec40eab4cd9db31ef481c816a6a89ada
BubblegumCombo <BubblegumCombo@users.noreply.github.com>
2020-04-30 14:22:35 -0700

    Initial commit

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+
+    $link = mysqli_connect("localhost", "root", "", "users");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error!");
+            
+        }
+
+?>
+
+
+<!-- 
+db: lolplanner
+pass: 1vc7krkwto
+server: shareddb-t.hosting.stackcp.net
+db un: lolplanner-3133314862
---
+++ header.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+      
+      <style type="text/css">
+
+
+          
+      .container {
+      
+            text-align: center;
+            width: 400px;
+      }
+          
+      #homePageContainer {
+              
+            margin-top: 150px;
+          
+      }
+          
+          #containerLoggedInPage {
+              
+              margin-top: 60px;
+              
+          }
+          
+          
+      html { 
+          
+          background-color: grey;  
+          -webkit-background-size: cover;
+          -moz-background-size: cover;
+          -o-background-size: cover;
+          background-size: cover;
+          
+          }
+          
+          body {
+              
+              background: none;
+              color: #FFF;
+              
+          }
+          
+          #logInForm {
+              
+              display:none;
+              
+          }
+          
+          .toggleForms {
+              
+              font-weight: bold;
+              
+          }
+          
+          #diary {
+              margin-left: 80%;
+              width: 20%;
+              height: 80%;
+              
+              
+          }
+
+          #can {
+            
+            position: fixed;
+            background-image: url(sr.jpg);
+            background-repeat: no-repeat;
+            background-size: contain;
+
+
+          }
+
+          #colors {
+
+          }
+
+          #draggable {
+
+            position: fixed;
+            
+          }
+          
+          #bminion {
+
+            position:absolute; bottom: -460px; right: -1050px;
+          }
+
+          #rminion {
+
+            position:absolute; bottom: -490px; right: -1000px;
+          }
+
+          .drag {
+            position: absolute;
+            
+          }
+  
+
+          #yasuo {
+
+            position:absolute; bottom: -430px; right: -1050px;
+          }
+
+          #akali {
+
+            position:absolute; bottom: -483px; right: -1200px;
+          }
+
+
+      </style>
+  </head>
+  <body>
